83518d17074866b1c2df2566454c0bacf8926895
James Hazard <james.g.hazard@gmail.com>
2015-07-12 00:27:50 +0200

    initializing Compliance

---
+++ vendor/CommonAccord/cmacc/library/landing.php
@@ -0,0 +1,18 @@
+<p align="center"><img src="visual/cmacc-trans.png" style="width:35%" /></p>
+
+   <table><tr><td>Welcome to a subdomain of CommonAccord.  This site is a home for codification of the legal aspects of supply chains.  We also propose integration with P2P transaction systems.
+<br><br>
+
+   This website is for "compliance" documents.  Currently, this centers on an <a href="index.php?action=doc&file=FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">Anti-Bribery and Anti-Corruption Policy</a>
+   contributed by David Simon of Foley & Lardner.  (Click on "Source" for the components and on "GitHub" for the repository.)<br>   
+<br>
+
+<a href="index.php?action=list&file=/">Documents</a>:<br>
+<br><br>
+This is a subset of the docs from <a href="http://www.commonaccord.org">commonaccord.org</a>.<br>
+
+</td><td>
+
+<a class="twitter-timeline" href="https://twitter.com/CommonAccord/with_replies" data-widget-id="574817616360964096" width="600" height="300">Tweets by @CommonAccord</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</td></tr></table>
