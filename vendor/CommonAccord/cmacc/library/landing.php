2897945c1ba6b46c72c1903fa086c7122e270149
James Hazard <james.g.hazard@gmail.com>
2015-07-12 18:06:30 +0200

    better intro

--- vendor/CommonAccord/cmacc/library/landing.php
+++ vendor/CommonAccord/cmacc/library/landing.php
@@ -1,15 +1,38 @@
 <p align="center"><img src="visual/cmacc-trans.png" style="width:35%" /></p>
 
-   <table><tr><td>Welcome to a subdomain of CommonAccord.  This site is a home for codification of the legal aspects of supply chains.  We also propose integration with P2P transaction systems.
-<br><br>
+   <table><tr><td>
 
-   This website is for "compliance" documents.  Currently, this centers on an <a href="index.php?action=doc&file=FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">Anti-Bribery and Anti-Corruption Policy</a>
-   contributed by David Simon of Foley & Lardner.  (Click on "Source" for the components and on "GitHub" for the repository.)<br>   
+   <span title="Introduction">This website is for "compliance" documents.  Currently, this centers on an <a href="index.php?action=doc&file=FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">Anti-Bribery and Anti-Corruption Policy</a>
+   contributed by David Simon of Foley & Lardner.</span><br><br>
+   <span title="Go To David's Document">When you click on the first link below, you will see his document.</span>
+<ul><li>
+
+   <span title="Colored Open Points">In that "<b>Document</b>" view, it will have red <font color="red">{open points}</font>.</span><li>
+   <span title="Cursor Tells You Where Text Came From">If you <u>hover your cursor</u> over a part of the text and wait a moment, a tiny box will pop up and tell you the name of that span of text.</span>   <li>
+
+ <span title="Source Tab">Click on "<b>Source</b>" for the components.</span><li>
+
+<span title="Edit Tab"> "<b>Edit</b>" let's you play (save box at the bottom of the window).  Your changes are  temporary, wiped out by the next update of the site, unless you edit by "forking" on the GitHub repo or email your text to us at <a href="mailto:commonaccord@gmail.com">commonaccord@gmail.com</a>.</span><li>   
+
+<span title="Edit and Complete Tab"> "<b>Edit and Complete</b>" prompts you with the missing elements. </span><li>
+
+   <span title="Print Tab">"<b>Print</b>" is the same as "Document," except it <i>does not</i> have a heading, color open fields or mark text spans. Ready for printing and stripped of that metadata.</span><li>
+
+   <span title="Go To GitHub">"<b>GitHub</b>" takes you to the source code for the entire site, kept at the place where many engineers keep software source code. If you have a GitHub account, feel free to fork.  If you don't - they are free, easy to get and very useful. </span><li>
+
+   <span title="Up">From "Source" view, you have an up arrow to take you to other files.</span>
+
+</ul>
+
+   <span title="Go To David Simon's document:"><a href="index.php?action=doc&file=FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">Anti-Bribery and Anti-Corruption Policy</a> by David Simon of Foley & Lardner.</span><br><br>
+<br><a href="index.php?action=list&file=/">Folder with all the documents.</a><br>
 <br>
+<span title="Other Sites">Other sites: <ul><li>
+<a href="http://source.commonaccord.org">source.commonaccord.org</a> (Master Service Agreement)<li>
+   <a href="http://ga4gh.commonaccord.org">ga4gh.commonaccord.org</a> (Patient Consents in Genomic Research)<li>
+   <a href="http://fr.commonaccord.org">fr.commonaccord.org</a> (French)<li>
+   <a href="http://commonaccord.org">commonaccord.org</a> (General)</ul></span>
 
-<a href="index.php?action=list&file=/">Documents</a>:<br>
-<br><br>
-This is a subset of the docs from <a href="http://www.commonaccord.org">commonaccord.org</a>.<br>
 
 </td><td>
 
