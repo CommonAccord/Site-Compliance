d1294395d84ce3b5f70877a680c3c2739683fa54
James Hazard <james.g.hazard@gmail.com>
2015-07-14 08:39:42 +0200

    the text for landing page now is Cmacc -

---
+++ Doc/Website/landing.md
@@ -0,0 +1,39 @@
+
+Model.Root=<p align="center">{Logo}</p>{Table.sec}
+
+Logo=<img src="visual/cmacc-trans.png" style="width:35%" />
+
+Table.sec=<table><tr><td>{IntroContent}</td><td>{TwitterBlock.sec}</td></tr></table>
+
+IntroContent={Introduction.sec}<br><br>{DavidsDoc.sec}<br>{PageTabs.sec}<br>{DocumentsFolder.sec}<br><br>{OtherSites.sec}<br><br>{PageSource.sec}
+
+
+
+Introduction.sec=This website is for compliance documents.<br><br>Currently, it centers on an <a href="index.php?action=doc&file=FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">Anti-Bribery and Anti-Corruption Policy</a> contributed by David Simon of Foley & Lardner. We invite you to use it, make extensions, suggest changes and contribute documents that complement or compete with it.  We also invite you to work on other materials at other CommonAccord sites or clone a site.
+
+DavidsDoc.sec=When you click on the first link below, you will see David's document.
+
+PageTabs.sec=<ul><li>{DocumentTab.sec}<li>{SourceTab.sec}<li>{EditTab.sec}<li>{EditCompleteTab.sec}<li>{PrintTab.sec}<li>{GitHubTab.sec}</ul>
+
+DocumentTab.sec=<a href="index.php?action=doc&file=FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">Document</a> view has red <font color="red">{open points}</font>. If you <u>hover your cursor</u> over a part of the text and wait a moment, a tiny box will pop up and tell you the name of that span of text.
+
+SourceTab.sec=<a href="index.php?action=source&file=FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">Source</a> for the components and an up arrow to take you to other files.
+
+EditTab.sec=<a href="index.php?action=edit&file=FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">Edit</a> lets you play (save box at the bottom of the window).  Your changes are  temporary, wiped out by the next update of the site, unless you edit by "forking" on the GitHub repo or email your text to us at <a href="mailto:commonaccord@gmail.com">commonaccord@gmail.com</a>.</span>
+
+EditCompleteTab.sec=<a href="index.php?action=openedit&file=FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">Edit and Complete</a> prompts you with the missing elements.
+
+PrintTab.sec=<a href="index.php?action=print&file=FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">Print</a> is the same as "Document," except it <i>does not</i> have a heading, colored opens or marked text spans. Ready for printing and stripped of metadata.
+
+GitHubTab.sec=<a href="http://github.com/CommonAccord/Site-Compliance/blob/master/Doc/FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">GitHub</a> takes you to the source code for the entire site, kept at GitHub, where engineers keep software source code. If you have a GitHub account, feel free to fork.  If you don't - ask an engineer.  A GitHub account is useful and free.
+ 
+Examples.sec=Examples:<ul><li><span title="Go to Acme's Policy"><a href="index.php?action=source&file=/Deal/Acme-Policies/Acme-AntiBriberyPolicy_01-01.md">Acme's Anti-Bribery Policy</a> based on the model policy.</span><li><span title="Revise Acme's Policy"><a href="index.php?action=source&file=/Deal/Acme-Policies/Acme-AntiBriberyPolicy_01-02.md">Draft 2 of Acme's Policy</a> based on that first draft.</span></ul>
+
+DocumentsFolder.sec=<a href="index.php?action=list&file=/">Folder with all the documents.</a>
+
+OtherSites.sec=Other sites: <ul><li><a href="http://source.commonaccord.org">source.commonaccord.org</a> (Master Service Agreement)<li>   <a href="http://ga4gh.commonaccord.org">ga4gh.commonaccord.org</a> (Patient Consents in Genomic Research)<li><a href="http://fr.commonaccord.org">fr.commonaccord.org</a> (French)<li>   <a href="http://commonaccord.org">commonaccord.org</a> (General)</ul>
+
+TwitterBlock.sec=<a class="twitter-timeline" href="https://twitter.com/CommonAccord/with_replies" data-widget-id="574817616360964096" width="600" height="300">Tweets by @CommonAccord</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+
+PageSource.sec=<a href="index.php?action=source&file=Website/Landing.md">Page Source</a>
--- vendor/CommonAccord/cmacc/library/landing.php
+++ vendor/CommonAccord/cmacc/library/landing.php
@@ -1,45 +1,22 @@
-<p align="center"><img src="visual/cmacc-trans.png" style="width:35%" /></p>
+<body style="margin:40;padding:0">
+<?php
 
-   <table><tr><td>
+$lib_path = LIB_PATH;
 
-   <span title="Introduction">This website is for compliance documents.  Currently, it centers on an <a href="index.php?action=doc&file=FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">Anti-Bribery and Anti-Corruption Policy</a> contributed by David Simon of Foley & Lardner. 
+$dir="Website/Landing.md";
 
-   We invite you to use it, make extensions, suggest changes and contribute documents that complement or compete with it.  We also invite you work on other materials at other CommonAccord sites or clone a site.
-</span><br><br>
-   <span title="Go To David's Document">When you click on the first link below, you will see David's document.</span>
-<ul><li>
+$document = `perl $lib_path/parser.pl $path/$dir`;
 
-   <span title="Colored Open Points">In the "<a href="index.php?action=doc&file=FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">Document</a>" view, it will have red <font color="red">{open points}</font>.</span><li>
-   <span title="Cursor Tells You Where Text Came From">If you <u>hover your cursor</u> over a part of the text and wait a moment, a tiny box will pop up and tell you the name of that span of text.</span>   <li>
+$minDocLength = 1;
 
- <span title="Source Tab">Click on "<a href="index.php?action=source&file=FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">Source</a>" for the components and an up arrow to take you to other files.<li>
+if (strlen($document) > $minDocLength){ 
 
-<span title="Edit Tab"> "<a href="index.php?action=edit&file=FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">Edit</a>" let's you play (save box at the bottom of the window).  Your changes are  temporary, wiped out by the next update of the site, unless you edit by "forking" on the GitHub repo or email your text to us at <a href="mailto:commonaccord@gmail.com">commonaccord@gmail.com</a>.</span><li>   
-
-<span title="Edit and Complete Tab"> "<a href="index.php?action=openedit&file=FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">Edit and Complete</a>" prompts you with the missing elements. </span><li>
-
-   <span title="Print Tab">"<a href="index.php?action=print&file=FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">Print</a>" is the same as "Document," except it <i>does not</i> have a heading, colored opens or marked text spans. Ready for printing and stripped of metadata.</span><li>
-
-   <span title="Go To GitHub">"<a href="http://github.com/CommonAccord/Site-Compliance/blob/master/Doc/FoleyGRS/FCPAPolicy/Form/FCPA_01-02.md">GitHub</a>" takes you to the source code for the entire site, kept at GitHub, where many engineers keep software source code. If you have a GitHub account, feel free to fork.  If you don't - ask an engineer.  A GitHub account is free, easy and very useful. </span>
- <li>
-
-<span title="Go to Acme's Policy"><a href="index.php?action=source&file=/Deal/Acme-Policies/Acme-AntiBriberyPolicy_01-01.md">Acme's Anti-Bribery Policy</a>" based on the model policy.</span><li>
-<span title="Revise Acme's Policy"><a href="index.php?action=edit&file=/Deal/Acme-Policies/Acme-AntiBriberyPolicy_01-02.md">Edit Draft 2 of Acme's Policy</a>" based on the model policy.</span>
-
-</ul>
-
-<br>
-<br><span title="Top Level Folder"><a href="index.php?action=list&file=/">Folder with all the documents.</a></span><br>
-<br>
-<span title="Other Sites">Other sites: <ul><li>
-<a href="http://source.commonaccord.org">source.commonaccord.org</a> (Master Service Agreement)<li>
-   <a href="http://ga4gh.commonaccord.org">ga4gh.commonaccord.org</a> (Patient Consents in Genomic Research)<li>
-   <a href="http://fr.commonaccord.org">fr.commonaccord.org</a> (French)<li>
-   <a href="http://commonaccord.org">commonaccord.org</a> (General)</ul></span>
+//kludge to let html headers have formatting.
+ 
+  echo $document;}
+ else {
+   echo "Nothing to Show";
 
 
-</td><td>
-
-<a class="twitter-timeline" href="https://twitter.com/CommonAccord/with_replies" data-widget-id="574817616360964096" width="600" height="300">Tweets by @CommonAccord</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-</td></tr></table>
+}
+?>
